3858c6ca9501fabbd237408934e3f8e0d0eedef4
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-04-13 17:40:17 -0300

    feat(Teste): adicionar classe de teste.

---
+++ cavaleirosdozoodiaco/SaintTest.java
@@ -0,0 +1,41 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SaintTest{
+    @Test
+    public void vestirArmaduraDeixaArmaduraVestida () {
+    /* 
+       1. Arrange - Montagem dos dados de teste
+       2. Act - Invocar a ação a ser testada
+       3. Assert - Verificação dos resultados de teste.
+       */
+      //1
+      Armadura escorpiao = new Armadura("Escorpião", Categoria.OURO);
+      Saint milo = new Saint("Milo", escorpiao);
+      
+      //2
+      milo.vestirArmadura();
+      
+      //3
+      boolean resultado = milo.getArmaduraVestida();
+      assertEquals(true, resultado);
+    }
+    
+    @Test
+    public void naoVestirArmaduraDeixaArmaduraNaoVestida () {
+        Saint hyoga = new Saint("Hyoga", new Armadura("cisne", Categoria.BRONZE));
+        assertEquals(false, hyoga.getArmaduraVestida());
+    }
+    
+    
+    @Test 
+    public void aoCriarGeneroENaoInformado(){
+        Armadura churrumingas = new Armadura("Churrumingas", Categoria.OURO);
+        Genero genero;
+        Saint shalon = new Saint("Shalon", churrumingas);
+    }
+}
