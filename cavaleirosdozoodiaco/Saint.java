fa641c98ce84b778cdd38e675eba0c08b8489e5b
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-04-13 16:22:00 -0300

    feat(Saint): adicionar Saint.

---
+++ cavaleirosdozoodiaco/Saint.java
@@ -0,0 +1,16 @@
+public class Saint {
+    private String nome;
+    private Armadura armadura;
+    private boolean armaduraVestida;
+    
+    public Saint(String nome, Armadura armadura) {
+        
+        this.nome = nome;
+        this.armadura = armadura;
+    }
+    
+     public void vestirArmadura () {
+        armaduraVestida = true;
+    }
+    
+}
