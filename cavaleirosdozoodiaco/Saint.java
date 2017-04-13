0bc78bba43a6b8240a06d53e1d021ba51e0b8ddf
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-04-13 17:43:54 -0300

    feat(Cavaleiro): adicionar construtor para genero.

--- cavaleirosdozoodiaco/Saint.java
+++ cavaleirosdozoodiaco/Saint.java
@@ -2,7 +2,13 @@ public class Saint {
     private String nome;
     private Armadura armadura;
     private boolean armaduraVestida;
+    private Genero genero = Genero.NAO_INFORMADO;
     
+    public Saint(String nome, Armadura armadura, Genero genero) {
+        this.genero = genero;
+        this.nome = nome;
+        this.armadura = armadura;
+    }
     public Saint(String nome, Armadura armadura) {
         
         this.nome = nome;
