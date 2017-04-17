9747640bb9fb401fe361b393002967231ac35b22
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-04-17 15:18:57 -0300

    refactor(Batalha): refatorar casos de condição e variável com quebra de linha.

--- cavaleirosdozoodiaco/Batalha.java
+++ cavaleirosdozoodiaco/Batalha.java
@@ -1,21 +1,19 @@
 public class Batalha {
     private Saint saint1, saint2;  
-    
+    private final double dano = 10;
     public Batalha (Saint saint1, Saint saint2) {
-        this
-        .saint1 = saint1;
+        this.saint1 = saint1;
         this.saint2 = saint2;        
     }
     
     public void iniciar () {
         int categoria1 = saint1.getArmadura().getCategoria().getValor();
         int categoria2 = saint2.getArmadura().getCategoria().getValor();
-        if(categoria1 == categoria2) {
-            saint2.perderVida(10);            
-        } else if (categoria1 > categoria2) {
-            saint2.perderVida(10);
-        } else if (categoria1 < categoria2) {
-            saint1.perderVida(10);
-        }               
+        
+        if (categoria1 >= categoria2)
+           saint2.perderVida(dano);
+        else if (categoria1 < categoria2) 
+           saint1.perderVida(dano);
+                      
     }
 }
