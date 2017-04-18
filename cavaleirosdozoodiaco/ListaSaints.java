105b03248b2caf5d93a024e59f05f45cd17561ee
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-04-18 20:39:35 -0300

    feat(Cavaleiro): adicionar classe para ter uma lista de cavaleiros.

--- cavaleirosdozoodiaco/ListaSaints.java
+++ cavaleirosdozoodiaco/ListaSaints.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 public class ListaSaints {
-    ArrayList<Saint> listaSaints = new ArrayList <> ();
+    private ArrayList<Saint> listaSaints = new ArrayList <> ();
     
     public void adicionar(Saint saint) {
         listaSaints.add(saint);
@@ -34,12 +34,70 @@ public class ListaSaints {
     
     
     public ArrayList<Saint> buscarPorCategoria(Categoria categoria)  {
-        
+        ArrayList<Saint> saints = new ArrayList <> ();
          for(Saint saint : listaSaints){
             if(saint.getArmadura().getCategoria().getValor() == categoria.getValor()){
-                
+                saints.add(saint);
             }
         }
-        return null;
+        return saints;
     }
+    
+    public ArrayList<Saint> buscarPorStatus(Status status) {
+        ArrayList<Saint> saints = new ArrayList <> ();
+        for(Saint saint : listaSaints) {
+            if(saint.getStatus() == status) {
+                saints.add(saint);
+            }
+        }
+        return saints;
+    }
+    
+    public Saint getSaintMaiorVida () {
+        double maiorVida = -1;
+        Saint saintMaiorVida = null;
+        for(Saint saint : listaSaints){
+            if(saint.getVida() > maiorVida) {
+                maiorVida = saint.getVida();
+                saintMaiorVida = saint;                
+            }
+            
+        }
+        return saintMaiorVida;
+    }
+    
+    public Saint getSaintMenorVida () {
+        double menorVida = 101;
+        Saint saintMenorVida = null;
+        for(Saint saint : listaSaints){
+            if(saint.getVida() < menorVida) {
+                menorVida = saint.getVida();
+                saintMenorVida = saint;                
+            }
+            
+        }
+        return saintMenorVida;
+    }
+    
+    public void ordenar() {              
+        ListaSaints listaAuxiliar = new ListaSaints();  
+        ListaSaints saintsOrdenados = new ListaSaints();
+        ArrayList<Saint> auxiliar = this.listaSaints;
+        int tamanho = auxiliar.size();
+        
+        for(Saint saintAuxiliar : auxiliar) {            
+            listaAuxiliar.adicionar(saintAuxiliar);
+        }               
+        
+        for(int i = 0; i < tamanho; i++){
+            Saint saintMenorVida;            
+            saintMenorVida = listaAuxiliar.getSaintMenorVida();            
+            saintsOrdenados.adicionar(saintMenorVida);            
+            listaAuxiliar.remover(saintMenorVida);
+        }
+        
+        this.listaSaints = saintsOrdenados.todos();
+    }
+    
+    
 }
---
+++ cavaleirosdozoodiaco/ListaSaintsTest.java
@@ -0,0 +1,170 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
+public class ListaSaintsTest {
+    @Test
+    public void adicionarSaint () throws Exception{
+        ListaSaints saints = new ListaSaints();
+        Saint saintTest = new Saint("Shyriu", new Armadura(new Constelacao("Pégaso"), Categoria.BRONZE));
+        
+        saints.adicionar(saintTest);
+        
+        assertEquals(saintTest, saints.get(0));
+    }
+    
+    @Test
+    public void pegarTodosSaints () throws Exception {
+        ListaSaints saints = new ListaSaints();
+        ArrayList<Saint> arrayTest = new ArrayList<> ();
+        Saint saintTest = new Saint("Shyriu", new Armadura(new Constelacao("Pégaso"), Categoria.BRONZE));
+        Saint saintTest2 = new Saint("Shyriu", new Armadura(new Constelacao("Pégaso"), Categoria.BRONZE));
+        Saint saintTest3 = new Saint("Shyriu", new Armadura(new Constelacao("Pégaso"), Categoria.BRONZE));
+        
+        saints.adicionar(saintTest);
+        saints.adicionar(saintTest2);
+        saints.adicionar(saintTest3);
+        arrayTest.add(saintTest);
+        arrayTest.add(saintTest2);
+        arrayTest.add(saintTest3);
+        
+        assertEquals(saints.todos(), arrayTest);
+    }
+    
+    
+    @Test
+    public void removerSaint () throws Exception {
+        ListaSaints saints = new ListaSaints();
+        Saint saintTest = new Saint("Shyriu", new Armadura(new Constelacao("Pégaso"), Categoria.BRONZE));
+        Saint saintTest2 = new Saint("Shyriu", new Armadura(new Constelacao("Pégaso"), Categoria.BRONZE));
+        saints.adicionar(saintTest);
+        saints.adicionar(saintTest2);
+        
+        saints.remover(saintTest);
+        
+        assertEquals(saints.todos().size(), 1);
+    }
+    
+    @Test
+    public void buscarSaintPorNome () throws Exception {
+        ListaSaints saints = new ListaSaints();
+        Saint saintTest = new Saint("Shyriu", new Armadura(new Constelacao("Pégaso"), Categoria.BRONZE));
+        Saint saintTest2 = new Saint("Shalon", new Armadura(new Constelacao("Pégaso"), Categoria.BRONZE));
+        saints.adicionar(saintTest);
+        saints.adicionar(saintTest2);
+        
+        assertEquals(saints.buscarPorNome("Shyriu"), saints.get(0));
+    }
+    
+    @Test
+    public void buscarSaintsPorCategoria () throws Exception {
+        ListaSaints saints = new ListaSaints();
+        ArrayList<Saint> arrayTest = new ArrayList<> ();
+        Saint saintTest = new Saint("Shyriu", new Armadura(new Constelacao("Pégaso"), Categoria.OURO));
+        Saint saintTest2 = new Saint("Shalon", new Armadura(new Constelacao("Pégaso"), Categoria.BRONZE));
+        Saint saintTest3 = new Saint("Shalon", new Armadura(new Constelacao("Pégaso"), Categoria.PRATA));
+        Saint saintTest4 = new Saint("Shalon", new Armadura(new Constelacao("Pégaso"), Categoria.BRONZE));
+        saints.adicionar(saintTest);
+        saints.adicionar(saintTest2);
+        saints.adicionar(saintTest3);
+        saints.adicionar(saintTest4);
+        arrayTest.add(saintTest2);
+        arrayTest.add(saintTest4);
+        
+        assertEquals(saints.buscarPorCategoria(Categoria.BRONZE), arrayTest);
+        
+        
+    }
+    
+      
+    @Test
+    public void buscarSaintsPorStatus () throws Exception {
+        ListaSaints saints = new ListaSaints();
+        ArrayList<Saint> arrayTest = new ArrayList<> ();
+        Saint saintTest = new Saint("Shyriu", new Armadura(new Constelacao("Pégaso"), Categoria.OURO));
+        Saint saintTest2 = new Saint("Shalon", new Armadura(new Constelacao("Pégaso"), Categoria.BRONZE));
+        Saint saintTest3 = new Saint("Shalon", new Armadura(new Constelacao("Pégaso"), Categoria.PRATA));
+        Saint saintTest4 = new Saint("Shalon", new Armadura(new Constelacao("Pégaso"), Categoria.BRONZE));
+        saintTest.perderVida(100);
+        saintTest3.perderVida(100);
+        saints.adicionar(saintTest);
+        saints.adicionar(saintTest2);
+        saints.adicionar(saintTest3);
+        saints.adicionar(saintTest4);
+        arrayTest.add(saintTest);
+        arrayTest.add(saintTest3);
+        
+        assertEquals(saints.buscarPorStatus(Status.MORTO), arrayTest);
+        
+        
+    }
+    
+    @Test
+    public void pegarSaintMaiorVida () throws Exception {
+        ListaSaints saints = new ListaSaints();
+        
+        Saint saintTest = new Saint("Shyriu", new Armadura(new Constelacao("Pégaso"), Categoria.OURO));
+        Saint saintTest2 = new Saint("Shalon", new Armadura(new Constelacao("Pégaso"), Categoria.BRONZE));
+        Saint saintTest3 = new Saint("Shalon", new Armadura(new Constelacao("Pégaso"), Categoria.PRATA));
+        Saint saintTest4 = new Saint("Shalon", new Armadura(new Constelacao("Pégaso"), Categoria.BRONZE));
+        saintTest.perderVida(32);
+        saintTest3.perderVida(74);
+        saintTest2.perderVida(1);        
+        saints.adicionar(saintTest);
+        saints.adicionar(saintTest2);
+        saints.adicionar(saintTest3);
+        saints.adicionar(saintTest4);        
+        
+        assertEquals(saints.getSaintMaiorVida().getVida(), 100, 0.001);
+        
+        
+    }
+   
+    @Test
+    public void pegarSaintMenorVida () throws Exception {
+        ListaSaints saints = new ListaSaints();
+        
+        Saint saintTest = new Saint("Shyriu", new Armadura(new Constelacao("Pégaso"), Categoria.OURO));
+        Saint saintTest2 = new Saint("Shalon", new Armadura(new Constelacao("Pégaso"), Categoria.BRONZE));
+        Saint saintTest3 = new Saint("Shalon", new Armadura(new Constelacao("Pégaso"), Categoria.PRATA));
+        Saint saintTest4 = new Saint("Shalon", new Armadura(new Constelacao("Pégaso"), Categoria.BRONZE));
+        saintTest.perderVida(32);
+        saintTest3.perderVida(74);
+        saintTest2.perderVida(1);        
+        saints.adicionar(saintTest);
+        saints.adicionar(saintTest2);
+        saints.adicionar(saintTest3);
+        saints.adicionar(saintTest4);        
+        
+        assertEquals(saints.getSaintMenorVida().getVida(), 26, 0.001);
+        
+        
+    }
+    
+    
+    @Test
+    public void ordenarSaints () throws Exception {
+        ListaSaints saints = new ListaSaints();
+        
+        Saint saintTest = new Saint("Shyriu", new Armadura(new Constelacao("Pégaso"), Categoria.OURO));
+        Saint saintTest2 = new Saint("Shalon", new Armadura(new Constelacao("Pégaso"), Categoria.BRONZE));
+        Saint saintTest3 = new Saint("Shalon", new Armadura(new Constelacao("Pégaso"), Categoria.PRATA));
+        Saint saintTest4 = new Saint("Shalon", new Armadura(new Constelacao("Pégaso"), Categoria.BRONZE));
+        saintTest.perderVida(99);
+        saintTest2.perderVida(98);
+        saintTest3.perderVida(97);   
+        saintTest4.perderVida(10);
+        saints.adicionar(saintTest);       
+        saints.adicionar(saintTest2);
+        saints.adicionar(saintTest3);
+        saints.adicionar(saintTest4);        
+        
+        saints.ordenar();
+        
+        assertEquals(saints.get(0), saints.getSaintMenorVida());
+        assertEquals(saints.get(3), saints.getSaintMaiorVida());
+    }
+}
