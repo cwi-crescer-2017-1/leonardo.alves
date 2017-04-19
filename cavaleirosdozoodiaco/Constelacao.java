b9c886b656666ef32c91397fc868a1c36ab4cca8
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-04-19 14:21:47 -0300

    refactor(Constelacao): remover espaço em branco.

--- cavaleirosdozoodiaco/Constelacao.java
+++ cavaleirosdozoodiaco/Constelacao.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 public class Constelacao {
     private String nome;
-    private ArrayList<Golpe> golpes = new ArrayList<> ();    
+    private ArrayList<Golpe> golpes = new ArrayList<> ();
+    
     public Constelacao(String nome) {
         this.nome = nome;
-
     }    
 
     public void adicionarGolpe(Golpe golpe) {
