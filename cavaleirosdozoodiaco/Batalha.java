6f36ce7849bb54eb14bf9af20576d5898ca55f16
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-04-21 18:31:27 -0300

    refactor(Batalha): refatorar construtor.

--- cavaleirosdozoodiaco/Batalha.java
+++ cavaleirosdozoodiaco/Batalha.java
@@ -1,11 +1,7 @@
 public class Batalha {
-    private Saint saint1, saint2;  
-    private final double dano = 10;
+    private Saint saintAtacaPrimeiro, saintAtacaDepois; 
+
     public Batalha (Saint saint1, Saint saint2) {
-        this.saint1 = saint1;
-        this.saint2 = saint2;        
-    }
-    
     public void iniciar () {
         int categoria1 = saint1.getArmadura().getCategoria().getValor();
         int categoria2 = saint2.getArmadura().getCategoria().getValor();
@@ -15,5 +11,14 @@ public class Batalha {
         else if (categoria1 < categoria2) 
            saint1.perderVida(dano);
                       
+        int categoria2 = saint2.getArmadura().getCategoria().getValor(); 
+        if(categoria1 >= categoria2) {
+            this.saintAtacaPrimeiro = saint1;
+            this.saintAtacaDepois = saint2;
+        } else {
+            this.saintAtacaPrimeiro = saint2;
+            this.saintAtacaDepois = saint1;
+        }               
+    }
     }
 }
