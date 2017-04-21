935b3b77ac08ce1ee09c8c08ddfc795aae7bdad7
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-04-21 18:36:35 -0300

    feat(Golpear): adicionar classe de teste que faltava.

---
+++ cavaleirosdozoodiaco/GolpearTest.java
@@ -0,0 +1,61 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GolpearTest {
+    @Test 
+    public void golpearSemArmaduraVestida() throws Exception {
+        Saint saintTest = new BronzeSaint("Shyriu", "Áries");
+        Saint saintTest2 = new BronzeSaint("Shyriu", "Áries");
+        Golpe golpe = new Golpe("Paulada", 10);
+        saintTest.aprenderGolpe(golpe);
+        Golpear golpear = new Golpear(saintTest, saintTest2);
+        golpear.executar();
+
+        assertEquals(100,saintTest.getVida(), 0.01);
+        assertEquals(90,saintTest2.getVida(), 0.01);
+    }    
+    
+    @Test
+    public void bronzeSaintGolpearComArmaduraMultiplicaPor2 () throws Exception {
+        Saint saintTest = new BronzeSaint("Shyriu", "Áries");
+        Saint saintTest2 = new BronzeSaint("Shyriu", "Áries");
+        Golpe golpe = new Golpe("Paulada", 10);
+        saintTest.vestirArmadura();
+        saintTest.aprenderGolpe(golpe);
+        Golpear golpear = new Golpear(saintTest, saintTest2);
+        golpear.executar();
+
+        assertEquals(100,saintTest.getVida(), 0.01);
+        assertEquals(80,saintTest2.getVida(), 0.01);
+    }
+    
+    @Test
+    public void silverSaintGolpearComArmaduraMultiplicaPor3 () throws Exception {
+        Saint saintTest = new SilverSaint("Shyriu", "Áries");
+        Saint saintTest2 = new BronzeSaint("Shyriu", "Áries");
+        Golpe golpe = new Golpe("Paulada", 10);
+        saintTest.vestirArmadura();
+        saintTest.aprenderGolpe(golpe);
+        Golpear golpear = new Golpear(saintTest, saintTest2);
+        golpear.executar();
+
+        assertEquals(100,saintTest.getVida(), 0.01);
+        assertEquals(70,saintTest2.getVida(), 0.01);
+    }
+    
+    @Test
+    public void goldSaintGolpearComArmaduraMultiplicaPor4 () throws Exception {
+        Saint saintTest = new GoldSaint("Shyriu", "Áries");
+        Saint saintTest2 = new BronzeSaint("Shyriu", "Áries");
+        Golpe golpe = new Golpe("Paulada", 10);
+        saintTest.vestirArmadura();
+        saintTest.aprenderGolpe(golpe);
+        Golpear golpear = new Golpear(saintTest, saintTest2);
+        golpear.executar();
+
+        assertEquals(100,saintTest.getVida(), 0.01);
+        assertEquals(60,saintTest2.getVida(), 0.01);
+    }
+}
