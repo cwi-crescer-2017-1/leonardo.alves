03e0743ffc62272f0eb3b9582f62ef8392cb069f
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-04-24 15:14:27 -0300

    test(VestirArmadura): adicionar classe de teste.

---
+++ cavaleirosdozoodiaco/VestirArmaduraTest.java
@@ -0,0 +1,30 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class VestirArmaduraTest {
+    @Test
+    public void movimentoVestirArmaduraRealmenteFunciona () throws Exception {
+        GoldSaint jackieChan = new GoldSaint ("Jackie", "Áries");
+        Movimento vestirArmadura = new VestirArmadura(jackieChan);
+        vestirArmadura.executar();
+        assertTrue(jackieChan.getArmaduraVestida());
+    }
+
+    @Test
+    public void naoVestirArmadura () throws Exception {
+        GoldSaint jackieChan = new GoldSaint ("Jackie", "Áries");
+        Movimento vestirArmadura = new VestirArmadura(jackieChan);        
+        assertFalse(jackieChan.getArmaduraVestida());
+    }
+    
+    @Test(expected=NullPointerException.class)
+    public void vestirArmaduraComSaintNull () throws Exception {
+        GoldSaint jackieChan = null;
+        Movimento vestirArmadura = new VestirArmadura(jackieChan);        
+        vestirArmadura.executar();
+    }
+}
