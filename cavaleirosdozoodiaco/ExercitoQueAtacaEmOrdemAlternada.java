9ca85cd10c2e81b23eb49d440110ce51c588409b
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-04-28 01:24:56 -0300

    refactor(ExercitoAlternado): substituir instanciação de variável para utilizar a da super classe.

--- cavaleirosdozoodiaco/ExercitoQueAtacaEmOrdemAlternada.java
+++ cavaleirosdozoodiaco/ExercitoQueAtacaEmOrdemAlternada.java
@@ -3,7 +3,7 @@ public class ExercitoQueAtacaEmOrdemAlternada extends ExercitoDeSaints {
     public Saint proximoSaint(){
         for(int i = 0; i < 3; i++){
             if(existeSaintNaCategoria()){            
-                Saint proximoSaint = getProximoSaintNaCategoria();
+                super.proximoSaint = getProximoSaintNaCategoria();
                 
                 removerSaintDaCategoria(proximoSaint);
                 
