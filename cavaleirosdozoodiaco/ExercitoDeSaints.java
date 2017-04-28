eeabedcf5db016b51d3d360cd61a727d52ab34ca
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-04-28 00:12:06 -0300

    feat(ExercitoDeSaints): adicionar método que vai para o próximo valor de categoria.

--- cavaleirosdozoodiaco/ExercitoDeSaints.java
+++ cavaleirosdozoodiaco/ExercitoDeSaints.java
@@ -1,5 +1,14 @@
 import java.util.ArrayList;
 public abstract class ExercitoDeSaints extends ListaSaints{    
+public abstract class ExercitoDeSaints extends ListaSaints{  
+    
+    protected int valorCategoriaSaint = 1;   
+    
+    protected void proximoValorCategoria () {
+        if(this.valorCategoriaSaint < 3) this.valorCategoriaSaint++; 
+            else this.valorCategoriaSaint = 1;
+          
+    }
     
     public void alistar(Saint saint) {
         adicionar(saint);
