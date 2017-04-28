c88e568d11894a18ff2f1e4e74f3d5f227ba4a5d
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-04-28 00:16:48 -0300

    feat(ExercitoHierarquico): adicionar lógica.

--- cavaleirosdozoodiaco/ExercitoQueAtacaEmOrdemHierarquica.java
+++ cavaleirosdozoodiaco/ExercitoQueAtacaEmOrdemHierarquica.java
@@ -1,18 +1,17 @@
-public class ExercitoQueAtacaEmOrdemHierarquica extends ExercitoDeSaints {
-    public Saint proximoSaint () {
-		/* to do
-		Saint proximoSaint = buscarPorCategoria(Categoria.BRONZE).get(0);
-		remover(buscarPorCategoria(Categoria.BRONZE).get(0));
-
-		if(proximoSaint == null){
-			proximoSaint = buscarPorCategoria(Categoria.PRATA).get(0);
-			remover(buscarPorCategoria(Categoria.PRATA).get(0));
-		}
-		if(proximoSaint == null){
-			proximoSaint = buscarPorCategoria(Categoria.OURO).get(0);
-			remover(buscarPorCategoria(Categoria.OURO).get(0));
-		}
-        return proximoSaint; */
-		return null;
+public class ExercitoQueAtacaEmOrdemHierarquica extends ExercitoDeSaints {        
+    
+    public Saint proximoSaint () {     
+        
+        for(int i = 0; i < 3; i++){      
+            
+            if(existeSaintNaCategoria()) {
+                super.proximoSaint = getProximoSaintNaCategoria();
+                removerSaintDaCategoria(proximoSaint);
+                
+                return proximoSaint;
+            } else 
+                proximoValorCategoria();
+        }
+        return null;
     }
 }
