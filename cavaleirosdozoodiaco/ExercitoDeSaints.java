065e7cb69904b406f4b29fbcdb2ca62f7096903f
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-04-28 00:12:49 -0300

    feat(ExercitoDeSaints): adicionar método para pegar o próximo saint na categoria escolhida.

--- cavaleirosdozoodiaco/ExercitoDeSaints.java
+++ cavaleirosdozoodiaco/ExercitoDeSaints.java
@@ -3,6 +3,7 @@ public abstract class ExercitoDeSaints extends ListaSaints{
 public abstract class ExercitoDeSaints extends ListaSaints{  
     
     protected int valorCategoriaSaint = 1;   
+    protected Saint proximoSaint;    
     
     protected void proximoValorCategoria () {
         if(this.valorCategoriaSaint < 3) this.valorCategoriaSaint++; 
@@ -10,6 +11,11 @@ public abstract class ExercitoDeSaints extends ListaSaints{
           
     }
     
+    
+    protected Saint getProximoSaintNaCategoria () {
+        
+        return buscarPorCategoria(this.valorCategoriaSaint).get(0);
+    }
     public void alistar(Saint saint) {
         adicionar(saint);
     }
