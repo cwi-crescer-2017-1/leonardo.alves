835f6661096f0a3611771c6e900fb27b95acb7ff
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-05-12 21:57:46 -0300

    feat(js-ex02): adicionar a funcao series invalidas.

---
+++ javascript/ex02/ex02.js
@@ -0,0 +1,18 @@
+function seriesInvalidas(series) {
+    let data = new Date(),
+        seriesInvalidas = [];
+    for (serie of series) {
+        if (serie.anoEstreia > data.getFullYear()) {
+            seriesInvalidas.push(serie);
+        } else {
+            let propriedadeSeries = Object.getOwnPropertyDescriptors(serie);
+            for (propriedade in propriedadeSeries) {
+                let valorProp = propriedadeSeries[propriedade].value;               
+                if (valorProp === "undefined" || valorProp === null) {
+                    seriesInvalidas.push(serie);
+                }
+            }
+        }
+    }
+    return seriesInvalidas;
+}
