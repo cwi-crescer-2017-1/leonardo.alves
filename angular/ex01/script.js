6072986501cabd140cb920725d714afe783c6af5
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-05-17 22:33:04 -0300

    feat(angular-ex01): adicionar funcao para gerar o pokemon a partir do id

--- angular/ex01/script.js
+++ angular/ex01/script.js
@@ -1,11 +1,35 @@
 var app = angular.module('pokemon', []);
 
+
 app.controller("controller", function ($scope) {
+    let dPoke = addPoke();
     // $scope.pokemons = [
     //     {nome: "Bulbassauro", tipo: "Grama"},
     //     {nome: "Charmander", tipo: "Fogo"},
     //     {nome: "Squirtle", tipo: "Água"}
     // ];
     $scope.pokemons = pokenames;
-    
+    $scope.objPokemons = dPoke;
 });
+
+var tipoAleatorio = function (obj) {
+    var keys = Object.keys(obj)
+    return obj[keys[keys.length * Math.random() << 0]];
+};
+
+
+function addPoke() {
+    let pokemons = [];
+    let tipos = ["fire", "grass", "water", "psychic", "ground", "normal",
+        "fighting", "flying", "poison", "electric", "rock", "ice", "bug", 
+        "dragon", "ghost", "dark", "steel", "fairy", "???"
+    ];
+    for (let i = 1; i <= 200; i++) {
+        pokemons.push({
+            nome: "Pokemon " + i,
+            tipo: " Tipo: " + tipoAleatorio(tipos),
+            id: i
+        });
+    }
+    return pokemons;
+}
