14da85f5cbdb34cadb3fccdfeaaf5f9a5c1926c1
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-05-22 21:37:15 -0300

    feat(crud.2): adicionar arquivo de config de rotas.

---
+++ angular/crud.2/js/config.js
@@ -0,0 +1,18 @@
+var crudApp = angular.module("crudApp", ["ngRoute"]);
+
+crudApp.config(function ($routeProvider){
+    $routeProvider
+        .when("/aulas", {
+            controller: "aulaController",
+            templateUrl: "aulas.html"
+        })
+        .when("/aulas/:idAula", {
+            controller: "aulaController",
+            templateUrl: "aulas.html"
+        })
+        .when("/instrutores", {
+            controller: "instrutorController",
+            templateUrl: "instrutores.html"
+        })
+        .otherwise({redirectTo: "/aulas"});
+});
