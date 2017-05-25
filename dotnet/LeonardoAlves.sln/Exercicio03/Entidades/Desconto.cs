53ef2bb318b51f8bb1dd701f9cd6f6c2e139fa0e
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-05-25 00:26:51 -0300

    feat(dotnet-ex03): adicionar classe de desconto.

--- dotnet/LeonardoAlves.sln/Exercicio03/Entidades/Desconto.cs
+++ dotnet/LeonardoAlves.sln/Exercicio03/Entidades/Desconto.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Exercicio03.Entidades
 {
-    class Desconto
+    public class Desconto
     {
+        
+        public Desconto(double aliquota, double valor)
+        {
+            Aliquota = aliquota;
+            Valor = valor;
+            ValorDesconto = Math.Round(Aliquota * Valor,2);
+            Restante = Math.Round(Valor - ValorDesconto,2);
+        }
+
+        public double Aliquota { get; private set; }
+        public double Valor { get; private set; }
+
+        public double ValorDesconto { get; private set; }
+
+        public double Restante { get; private set; }
     }
 }
