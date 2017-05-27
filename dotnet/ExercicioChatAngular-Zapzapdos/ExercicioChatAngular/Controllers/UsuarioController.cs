2e48676e2ae54ec3d97da97710b85452d9249d13
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-05-27 02:10:28 -0300

    feat(chat): adicionar controller do usuario.

---
+++ dotnet/ExercicioChatAngular-Zapzapdos/ExercicioChatAngular/Controllers/UsuarioController.cs
@@ -0,0 +1,41 @@
+﻿using ExercicioChatAngular.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace ExercicioChatAngular.Controllers
+{
+    public class UsuarioController : ApiController
+    {
+        private static object objLock = new object();
+
+        public static int idUser = 0;
+        private static List<Usuario> allUsers = new List<Usuario>();
+
+        public List<Usuario> Get (int? id = null)
+        {
+            if (id.HasValue)
+                return allUsers.Where(user => user.Id == id).ToList();
+            else
+                return allUsers;
+        }
+
+        public IHttpActionResult Post(Usuario user)
+        {
+            if (user.Id == 0)
+            {
+                lock (objLock)
+                {
+                    user.Id = ++idUser;
+                    allUsers.Add(user);
+                }
+                return Ok("Usuario entrou no canal com sucesso.");
+            }
+
+            else
+                return BadRequest("Algo inesperado aconteceu.");
+
+        }
+
+    }
+}
