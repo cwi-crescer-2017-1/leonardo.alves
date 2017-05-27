7d3a27eb52b34f1748f3cbec13c7cf4910d5aad0
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-05-27 02:09:42 -0300

    feat(chat): adicionar model da mensagem.

---
+++ dotnet/ExercicioChatAngular-Zapzapdos/ExercicioChatAngular/Models/Mensagem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExercicioChatAngular.Models
+{
+    public class Mensagem
+    {
+        public string Texto { get; set; }
+
+        public int Id { get; set; }
+
+        public DateTime HoraMensagem { get; set; } 
+
+        public int IdUsuario { get; set; }
+    }
+}
