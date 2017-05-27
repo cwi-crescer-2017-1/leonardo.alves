d4132cefa464ee73726bd172457db91346cf9d99
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-05-27 02:10:02 -0300

    feat(chat): adicionar model do usuario.

---
+++ dotnet/ExercicioChatAngular-Zapzapdos/ExercicioChatAngular/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExercicioChatAngular.Models
+{
+    public class Usuario
+    {
+        
+        public string Nome { get; set; }
+
+        public int Id { get; set; }
+        
+        public string UrlFoto { get; set; }
+       
+    }
+}
