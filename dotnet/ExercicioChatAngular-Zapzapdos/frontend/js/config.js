0871657108dc611892516100603611d131280c21
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-05-27 02:12:37 -0300

    feat(chat): adicionar javascript de configuração.

---
+++ dotnet/ExercicioChatAngular-Zapzapdos/frontend/js/config.js
@@ -0,0 +1,15 @@
+
+var app = angular.module("zapzapdos", ["ngRoute"]);
+
+app.config(function ($routeProvider, $locationProvider){
+    $routeProvider
+        .when("/chat", {
+            controller: "mensagemController",
+            templateUrl: "../html/chat.html"
+        })
+        .when("/welcome", {
+            controller: "usuarioController",
+            templateUrl: "../html/welcome.html"
+        })
+        .otherwise({redirectTo: "/welcome"});
+});
