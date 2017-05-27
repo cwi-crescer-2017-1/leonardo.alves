967c3656ffc8923151c8a8d721f2cbbdaf94bd37
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-05-27 02:10:43 -0300

    feat(chat): adicionar controller da mensagem.

---
+++ dotnet/ExercicioChatAngular-Zapzapdos/ExercicioChatAngular/Controllers/MensagemController.cs
@@ -0,0 +1,44 @@
+﻿using ExercicioChatAngular.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExercicioChatAngular.Controllers
+{    
+    public class MensagemController : ApiController
+    {
+        private static object objLock = new object();
+
+        private static int idMensagem = 0;
+        private static List<Mensagem> allMessages = new List<Mensagem>();
+
+        public List<Mensagem> Get ()
+        {            
+            return allMessages;            
+        }
+
+
+        public IHttpActionResult Post (Mensagem mensagem)
+        {
+            if (mensagem.IdUsuario > UsuarioController.idUser)
+                return BadRequest("Impossível enviar mensagem, pois há um erro no id do comunicador.");
+
+            if(mensagem.Id == 0)
+            {
+                lock (objLock)
+                {
+                    mensagem.Id = ++idMensagem;
+                    allMessages.Add(mensagem);
+                }
+                return Ok("Mensagem enviada.");
+            } 
+            else
+            {
+                return BadRequest("Você não deve definir um id para a mensagem.");
+            }
+        }
+    }
+}
