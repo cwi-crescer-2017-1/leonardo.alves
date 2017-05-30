2fff8987ee924dfeb3d9a31dc110ce99773e0d0e
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-05-30 17:40:50 -0300

    feat(dotnet): adicionar arquivos da aula 05.

---
+++ dotnet/Aula05/EditoraCrescer/EditoraCrescer.Infraestrutura/Mappings/AutorMap.cs
@@ -0,0 +1,21 @@
+﻿using EditoraCrescer.Infraestrutura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditoraCrescer.Infraestrutura.Mappings
+{
+    public class AutorMap : EntityTypeConfiguration<Autor>
+    {
+        public AutorMap()
+        {
+            ToTable("Autores");
+
+            HasKey(x => x.Id);
+
+        }
+    }
+}
---
+++ dotnet/Aula05/EditoraCrescer/EditoraCrescer.Infraestrutura/Repositorios/LivroRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using EditoraCrescer.Infraestrutura.Entidades;
+using EditoraCrescer.Infraestrutura.Contexto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditoraCrescer.Infraestrutura.Repositorios
+{
+
+    public class LivroRepositorio
+    {
+        private Contexto.Contexto contexto = new Contexto.Contexto();
+        private List<Livro> Obter()
+        {
+            return contexto.Livros.ToList();
+        }
+    }
+}
