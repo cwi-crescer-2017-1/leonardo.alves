61042632f3d454489c36771bd474dd18e21163f5
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-05-31 17:37:19 -0300

    feat(repositorio): adicionar obter por ibsn e atualizar.

--- dotnet/Aula05/EditoraCrescer/EditoraCrescer.Infraestrutura/Repositorios/LivroRepositorio.cs
+++ dotnet/Aula05/EditoraCrescer/EditoraCrescer.Infraestrutura/Repositorios/LivroRepositorio.cs
@@ -16,6 +16,16 @@ namespace EditoraCrescer.Infraestrutura.Repositorios
             return contexto.Livros.ToList();
         }
 
+        public Livro ObterPorIsbn (int isbn)
+        {
+            return contexto.Livros.FirstOrDefault(l => l.Isbn == isbn);
+        }
+        public List<Livro> ObterPorGenero(string genero)
+        {
+            return contexto.Livros
+                .Where(l => l.Genero.Contains(genero)).ToList();
+        }
+
         public void Criar (Livro livro)
         {
             if (livro.IdAutor == 0)
@@ -31,12 +41,27 @@ namespace EditoraCrescer.Infraestrutura.Repositorios
             contexto.SaveChanges();
         }
 
+        public bool AtualizarLivro (int isbn, Livro livro, out List<string> mensagens)
+        {           
+            mensagens = new List<string>();
+
+            verificarDadosAtualizacao(isbn, livro, mensagens);
+
+            if (mensagens.Count > 0) return false;
+
+            contexto.Entry(livro).State = System.Data.Entity.EntityState.Modified;
+            contexto.SaveChanges();
+
+            return true;
+        }        
+
         public void Deletar (int id)
         {
             var livro = contexto.Livros.FirstOrDefault(l => l.Isbn == id);
             contexto.Livros.Remove(livro) ;
         }
 
+       
 
         private int compararData (DateTime data)
         {
@@ -51,6 +76,19 @@ namespace EditoraCrescer.Infraestrutura.Repositorios
             if (compararData(livro.DataRevisao) < 1)
                 livro.DataRevisao = DateTime.Now;
         }
+
+        private void verificarDadosAtualizacao(int isbn, Livro livro, List<string> mensagens)
+        {
+            if (isbn < 1)
+                mensagens.Add("O isbn não pode ser negativo.");
+
+            if (contexto.Livros.Count(l => l.Isbn == isbn) > 0)
+                mensagens.Add("O isbn não existe no banco de dados.");
+
+            if (livro.Isbn != isbn)
+                mensagens.Add("O url não é compativel com o livro que você quer alterar.");
+        }
+
         public void Dispose ()
         {
             contexto.Dispose();
