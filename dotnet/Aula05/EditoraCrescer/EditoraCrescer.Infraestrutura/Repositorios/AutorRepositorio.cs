ca7ee2ff06c692f42a3e497957b06c66d7404432
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-05-31 23:30:14 -0300

    feat(autor): adicionar metodos para obter autor por id e livros no repositorio.

--- dotnet/Aula05/EditoraCrescer/EditoraCrescer.Infraestrutura/Repositorios/AutorRepositorio.cs
+++ dotnet/Aula05/EditoraCrescer/EditoraCrescer.Infraestrutura/Repositorios/AutorRepositorio.cs
@@ -10,20 +10,61 @@ namespace EditoraCrescer.Infraestrutura.Repositorios
     public class AutorRepositorio
     {
         private Contexto contexto = new Contexto();
-        public List<Autor> Obter ()
+        public List<Autor> ObterAutores ()
         {
            return contexto.Autores.ToList();
         }
 
+        public Autor ObterAutor(int id)
+        {            
+            return contexto.Autores.FirstOrDefault(a => a.Id == id);
+        }
+        
+        public List<Livro> ObterLivrosAutor(int id)
+        {
+            return contexto.Livros
+                .Where(l => l.IdAutor == id).ToList();
+        }
         public void Criar (Autor autor)
         {
             contexto.Autores.Add(autor);
         }
 
-        public void Deletar (int id)
+        public bool Alterar (int id, Autor autor, out List<string> mensagens)
         {
-            var autor = contexto.Autores.FirstOrDefault(x => x.Id == id);
+            mensagens = new List<string>();
+
+            verificarDadosAtualizacao(id, autor, mensagens);
+
+            if (mensagens.Count > 0) return false;
+
+            contexto.Entry(autor).State = System.Data.Entity.EntityState.Modified;
+            contexto.SaveChanges();
+
+            return true;
+        }
+
+        private void verificarDadosAtualizacao(int id, Autor autor, List<string> mensagens)
+        {
+            if (id < 1)
+                mensagens.Add("Id não pode ser negativo.");
+
+            if (!contexto.Autores.Any(a => a.Id == id))
+                mensagens.Add("O id do autor não confere com o id da URL.");
+
+            if (id != autor.Id)
+                mensagens.Add("Id's não conferem.");
+        }
+
+        public bool Deletar (int id, out Autor autor)
+        {
+            autor = contexto.Autores.FirstOrDefault(x => x.Id == id);
+            if (autor == null) return false;
+
             contexto.Autores.Remove(autor);
+            contexto.SaveChanges();
+
+            return true;
         }
 
         public void Dispose()
