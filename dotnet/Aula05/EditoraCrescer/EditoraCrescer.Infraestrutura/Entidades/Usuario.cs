2ca5cd9d5ff5d539235d1878df6f3f289473e39a
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-02 17:34:55 -0300

    feat(usuario): criar classe de usuario

---
+++ dotnet/Aula05/EditoraCrescer/EditoraCrescer.Infraestrutura/Entidades/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditoraCrescer.Infraestrutura.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+
+        public virtual ICollection<Permissoes> Permissoes { get; set; }
+    }
+}
---
+++ dotnet/Aula05/EditoraCrescer/EditoraCrescer.Infraestrutura/Mappings/UsuarioMap.cs
@@ -0,0 +1,21 @@
+﻿using EditoraCrescer.Infraestrutura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditoraCrescer.Infraestrutura.Mappings
+{
+    public class UsuarioMap : EntityTypeConfiguration<Usuario>
+    {
+        public UsuarioMap()
+        {
+            ToTable("Usuarios");
+
+            HasKey(x => x.Id);
+
+        }
+    }
+}
