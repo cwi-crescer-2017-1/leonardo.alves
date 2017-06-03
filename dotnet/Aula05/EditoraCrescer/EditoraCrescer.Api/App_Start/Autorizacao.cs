b147510506167f72eab63bc50aa5980ecf69a76f
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-03 01:19:46 -0300

    feat(autorização): adicionar boa parte do exemplo do Giovani com alguns ajustes.

--- dotnet/Aula05/EditoraCrescer/EditoraCrescer.Api/App_Start/Autorizacao.cs
+++ dotnet/Aula05/EditoraCrescer/EditoraCrescer.Api/App_Start/Autorizacao.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using EditoraCrescer.Infraestrutura.Entidades;
+using EditoraCrescer.Infraestrutura.Repositorios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Principal;
 using System.Text;
+using System.Threading;
 using System.Web;
 using System.Web.Http;
 using System.Web.Http.Controllers;
@@ -12,40 +16,87 @@ namespace EditoraCrescer.Api.App_Start
 {
     public class Autorizacao : AuthorizeAttribute
     {
+        UsuarioRepositorio _usuarioRepositorio = new UsuarioRepositorio();
+
         private static void negarAcesso(HttpActionContext actionContext)
         {
+            var dnsHost = actionContext.Request.RequestUri.DnsSafeHost;
+
             actionContext.Response =
                 actionContext
                 .Request
                 .CreateResponse(HttpStatusCode.Unauthorized);
+
+            actionContext
+                .Response
+                .Headers
+                .Add("WWW-Authenticate", string.Format("Basic realm=\"{0}\"", dnsHost));
         }
+
         public override void OnAuthorization(HttpActionContext actionContext)
         {
-            if (actionContext.Request.Headers.Authorization == null)
+            if (actionContext.Request.Headers.Authorization == null)            
                 negarAcesso(actionContext);
+
             else
             {
-                //obtém o parâmetro (token de autenticação)
+                
                 string tokenAutenticacao =
-                actionContext.Request.Headers.Authorization.Parameter;
-
-                // decodifica o parâmetro, pois ele deve vir codificado em base 64
+                    actionContext.Request.Headers.Authorization.Parameter;
+                
                 string decodedTokenAutenticacao =
                     Encoding.Default.GetString(Convert.FromBase64String(tokenAutenticacao));
-
-                // obtém o login e senha (usuario:senha)
-                string[] userNameAndPassword = decodedTokenAutenticacao.Split(':');
-
-                string usuario = userNameAndPassword[0];
-                string senha = userNameAndPassword[1];
-
-                if (!(usuario == "leo" && senha == "123456"))                
-                    negarAcesso(actionContext);                
-
-                //if (Roles != permissoes)
-                //    negarAcesso(actionContext);
                 
+                string[] userNameAndPassword = decodedTokenAutenticacao.Split(':');                
 
+                Usuario usuario = null;
+                if (ValidarUsuario(userNameAndPassword[0], userNameAndPassword[1], out usuario))
+                {
+                    string[] papeis = usuario.Permissoes.Select(papel => papel.Nome).ToArray();
+                    var identidade = new GenericIdentity(usuario.Email);
+                    var genericUser = new GenericPrincipal(identidade, papeis);
+
+                    // confere o perfil da action com os do usuário
+                    if (string.IsNullOrEmpty(Roles))
+                    {
+                        // atribui o usuário informado no contexto da requisição atual
+                        Thread.CurrentPrincipal = genericUser;
+                        if (HttpContext.Current != null)
+                            HttpContext.Current.User = genericUser;
+
+                        return;
+                    }
+                    else
+                    {
+                        var currentRoles = Roles.Split(',');
+                        foreach (var currentRole in currentRoles)
+                        {
+                            if (genericUser.IsInRole(currentRole))
+                            {
+                                // atribui o usuário informado no contexto da requisição atual
+                                Thread.CurrentPrincipal = genericUser;
+                                if (HttpContext.Current != null)
+                                    HttpContext.Current.User = genericUser;
+
+                                return;
+                            }
+                        }
+                    }
+                }
             }
+        }
+        private bool ValidarUsuario(string login, string senha, out Usuario usuarioRetorno)
+        {
+            usuarioRetorno = null;
+
+            var usuario = _usuarioRepositorio.Buscar(login);
+
+            if (usuario != null && usuario.ValidarSenha(senha))
+                usuarioRetorno = usuario;
+            else
+                usuario = null;
+
+            return usuario != null;
+        }
     }
 }
