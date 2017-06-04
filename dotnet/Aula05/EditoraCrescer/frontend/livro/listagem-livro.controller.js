f887c88b589a165c3aa033961e80f462920811d1
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-04 19:41:52 -0300

    refactor(home): trocar botões de pagina para um que adiciona conteudo.

--- dotnet/Aula05/EditoraCrescer/frontend/livro/listagem-livro.controller.js
+++ dotnet/Aula05/EditoraCrescer/frontend/livro/listagem-livro.controller.js
@@ -1,77 +1,39 @@
 app.controller("listagemLivroController", listagemLivroController);
 
-function listagemLivroController($scope, livroService){
-    $scope.parametros = {pegar: 6, pular: 0};
-    $scope.paginacao = {proximo: true, anterior: false};
-    $scope.proximaPagina = getLivros;
-    $scope.paginaAnterior = getLivros;    
+function listagemLivroController($scope, livroService) {
+    $scope.parametros = {
+        pegar: 6,
+        pular: 0
+    };
+    $scope.carregarLivros = getLivros;
+    $scope.haLivros = true;
+    $scope.livros = [];
     getLivros();
     getLivrosNovos();
 
     //timeout pro carroussel funcionar
     setTimeout(() => $('.slider').slider(), 1000);
 
-    function getLivros (){
-       return livroService.readAll($scope.parametros)
-            .then(response =>  {                   
-               /*
-                    se o servidor devolveu 6 livros, isso indica que há
-                    mais livros a serem entregues, então pode avançar de
-                    página. Se o servidor devolveu menos de 6 livros,
-                    quer dizer que ele tá na ultima página.
-               */
-                if(recebeuSeisLivros(response.data.data)){     
+    function getLivros() {
+        return livroService.readPublished($scope.parametros)
+            .then(response => {
+                if (response.data.dados.length > 0) {
+                    for (livro in response.data.dados)
+                        $scope.livros.push(response.data.dados[livro]);
 
-                    seraQueEstaNaPrimeiraPagina();
-
-                    avançarPagina();
-
-                    if($scope.parametros.pular !== 6)   //como a pagina so atualiza apos o clique, apenas mostra
-                         $scope.paginaAnterior = true;  // o botão de volta caso o pular seja diferente que o 
-                                                        //valor da primeira pagina (0 mas que agora é 6)
-
-                } else if (aindaHaLivros(response.data.data)){                                          
-                    $scope.paginacao.anterior = true;               
-                    voltarPagina();             
-                    $scope.paginacao.proximo = false;                  
-                }               
-                
-                //substitui a view apenas se haver livros.
-                if(!!response.data.data) {
-                    $scope.livros = response.data.data;
-
-                    //vai pro inicio da pagina
-                    document.getElementById('inicio').scrollIntoView()
+                    $scope.parametros.pegar += 6;
+                    $scope.parametros.pular += 6;
+                } else {
+                     $scope.haLivros = false;
+                     Materialize.toast("Opa! Parece que você já viu todos os livros! :O", 4000, 'rounded');
                 }
             });
-    } 
-    function getLivrosNovos () {
+    }
+
+    function getLivrosNovos() {
         return livroService.readNews()
-            .then(response =>{$scope.livrosNovos = response.data.data});
-    } 
-
-    var haPaginaAnterior = () =>        
-            $scope.paginacao.anterior = $scope.parametros.pular > 0;   
-
-    function avançarPagina () {
-        $scope.parametros.pegar +=6;
-        $scope.parametros.pular +=6;  
+            .then(response => {
+                $scope.livrosNovos = response.data.dados
+            });
     }
-
-    var seraQueEstaNaPrimeiraPagina = () => {
-        if($scope.parametros.pular == 0) {
-            $scope.paginacao.proximo = true;                        
-            $scope.paginacao.anterior = false;                        
-        }    
-    }
-
-    function voltarPagina () {
-        $scope.parametros.pegar -=6;
-        $scope.parametros.pular -=6;  
-    }
-
-    var recebeuSeisLivros = (livrosRecebidos) => livrosRecebidos.length === 6;
-
-    var aindaHaLivros = (livrosRecebidos) => livrosRecebidos.length > 0;        
-    
 }
