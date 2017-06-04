d3fc41e0614784a9c3ca830a82edc1dc3bf976fb
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-04 19:43:24 -0300

    feat(usuario): adicionar função de cadastro.

--- dotnet/Aula05/EditoraCrescer/frontend/usuario/usuario.controller.js
+++ dotnet/Aula05/EditoraCrescer/frontend/usuario/usuario.controller.js
@@ -1,6 +1,6 @@
 app.controller("usuarioController", usuarioController);
 
-function usuarioController($scope, $rootScope,authService, livroService) {
+function usuarioController($scope, $rootScope,authService, livroService, usuarioService) {
     $scope.auth = authService;
     $scope.livros = [];
     $rootScope.isAutenticado = authService.isAutenticado;
@@ -10,8 +10,9 @@ function usuarioController($scope, $rootScope,authService, livroService) {
 
     $scope.getLivros = getLivros;
     $scope.login = login;
+    $scope.cadastrar = cadastrar;
     $scope.adicionarLivro = adicionarLivro;
-
+    
     getLivros();
     
     
@@ -22,14 +23,14 @@ function usuarioController($scope, $rootScope,authService, livroService) {
                 Materialize.toast('Logado com sucesso!', 1000, 'rounded');       
             },
             function (response) {  
-                Materialize.toast('Opa! Algo deu errado.', 1000, 'rounded');             
+                Materialize.toast(response.data.mensagem, 1000, 'rounded');             
             });
     }
 
      function getLivros (){
        return livroService.readAll($scope.parametros)
             .then(response =>  { 
-                for(livro of response.data.data){
+                for(livro of response.data.dados){
                     $scope.livros.push(livro);
                 }
                 
@@ -44,11 +45,22 @@ function usuarioController($scope, $rootScope,authService, livroService) {
             .then(response => {
                 Materialize.toast("Livro adicionado com sucesso!", 1500, 'rounded');
                 $scope.livro = [];
+                getLivros();
             }, fail => {
                 mensagens = "";
                 for(i in fail.data.mensagem) mensagens  + " , " + mensagem[i];
                
                 Materialize.toast(mensagens, 3000, 'rounded');
+            })
+    }
+
+    function cadastrar(usuario){
+        return usuarioService.create(usuario)
+            .then(response=> {
+                Materialize.toast("Cadastro efetuado com sucesso!", 1500, 'rounded');
+                login(usuario);
+            }, fail=>{
+                Materialize.toast("Opa, não foi possível efetuar o cadastro!", 3000, 'rounded');                
             })
     }
 }
