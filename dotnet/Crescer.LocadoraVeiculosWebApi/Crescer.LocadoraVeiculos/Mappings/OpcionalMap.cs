36b296516f940c925d9002501b4b8a3654317e89
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-05 23:08:42 -0300

    feat(opcionais): adicionar map de opcional.

---
+++ dotnet/Crescer.LocadoraVeiculosWebApi/Crescer.LocadoraVeiculos/Mappings/OpcionalMap.cs
@@ -0,0 +1,24 @@
+﻿using Crescer.LocadoraVeiculosDominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace Crescer.LocadoraVeiculos.Mappings
+{
+    public class OpcionalMap : EntityTypeConfiguration<Opcional>
+    {
+        public OpcionalMap()
+        {
+            ToTable("Opcional");
+
+            HasKey(x => x.Id);
+                       
+            Property(x => x.Preco).IsRequired();
+            Property(x => x.Quantidade).IsRequired();
+            Property(x => x.Descricao).IsRequired().HasMaxLength(400);
+
+        }
+    }
+}
