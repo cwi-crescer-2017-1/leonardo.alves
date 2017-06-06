f9e537f36c90281f74c6f4694faf44f022a9d385
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-06 16:04:37 -0300

    feat(cliente): adicionar metodo no repositorio para obter e cadastrar.

---
+++ dotnet/Crescer.LocadoraVeiculosWebApi/Crescer.LocadoraVeiculosInfraestrutura/Repositorio/ClienteRepositorio.cs
@@ -0,0 +1,27 @@
+﻿using Crescer.LocadoraVeiculos;
+using Crescer.LocadoraVeiculosDominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crescer.LocadoraVeiculosInfraestrutura.Repositorio
+{
+    public class ClienteRepositorio
+    {
+        Contexto contexto = new Contexto();
+
+        public Cliente Obter(string cpf)
+        {
+            return contexto.Clientes.FirstOrDefault(c => c.Cpf == cpf);
+        }
+
+        public void Cadastrar(Cliente cliente)
+        {
+            contexto.Clientes.Add(cliente);
+            contexto.SaveChanges();
+        }
+
+    }
+}
