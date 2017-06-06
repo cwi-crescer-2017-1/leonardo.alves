29ecf37b796c5dc6b4303ad2833c1807b8df4d57
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-05 23:10:02 -0300

    feat(pedido): adicionar map e outras propriedades relacionadas.

---
+++ dotnet/Crescer.LocadoraVeiculosWebApi/Crescer.LocadoraVeiculos/Mappings/PedidoMap.cs
@@ -0,0 +1,49 @@
+﻿using Crescer.LocadoraVeiculosDominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace Crescer.LocadoraVeiculos.Mappings
+{
+    public class PedidoMap : EntityTypeConfiguration<Pedido>
+    {
+        public PedidoMap()
+        {
+            ToTable("Pedido");
+
+            HasKey(x => x.Id);
+
+            Property(x => x.IdCliente).IsRequired();
+            Property(x => x.IdVeiculo).IsRequired();
+
+            Property(x => x.DataPedido).IsRequired();
+            Property(x => x.DataEntregaPrevista).IsRequired();
+
+            Property(x => x.DataEntregaReal).IsOptional();
+
+            HasRequired(x => x.Cliente)
+                .WithMany()
+                .HasForeignKey(x => x.IdCliente);
+
+            HasOptional(x => x.Pacote)
+                .WithMany()
+                .HasForeignKey(x => x.IdPacote);
+
+            HasRequired(x => x.Veiculo)
+                .WithMany()
+                .HasForeignKey(x => x.IdVeiculo);
+
+            HasMany(x => x.Opcionais)
+                .WithMany()
+                .Map(x =>
+                {
+                    x.MapLeftKey("IdPedido");
+                    x.MapRightKey("IdOpcional");
+                    x.ToTable("PedidoOpcional");
+                });
+        }
+
+    }
+}
--- dotnet/Crescer.LocadoraVeiculosWebApi/Crescer.LocadoraVeiculosDominio/Entidades/Pedido.cs
+++ dotnet/Crescer.LocadoraVeiculosWebApi/Crescer.LocadoraVeiculosDominio/Entidades/Pedido.cs
@@ -8,37 +8,42 @@ namespace Crescer.LocadoraVeiculosDominio.Entidades
 {
     public class Pedido
     {
-        public int Id { get; set; }
-
-        public int IdVeiculo { get; set; }
-
+        public int IdCliente { get; set; }
+        public Cliente Cliente { get; set; }
         public int? IdPacote { get; set; }
-
+        public Pacote Pacote { get; set;}
+        public List<Opcional> Opcionais { get; set; }
+        public int Id { get; set; }
+        public Veiculo Veiculo { get; set; }
+        public int IdVeiculo { get; set; }
         public DateTime DataPedido { get; set; }
-
         public DateTime DataEntregaPrevista { get; set; }
-
         public DateTime? DataEntregaReal { get; set; }
 
         protected Pedido () { }
 
-        public Pedido (DateTime dataEntregaPrevista)
+        public Pedido (DateTime dataEntregaReal)
         {
-            DataEntregaPrevista = dataEntregaPrevista;
+            //Construtor para setar a entrega do veículo
+            DataEntregaReal = dataEntregaReal;
         }
 
         public Pedido(int idVeiculo, DateTime dataPedido, DateTime dataEntregaPrevista)
         {
+            //Construtor caso não haja pacote
             IdVeiculo = idVeiculo;            
             DataPedido = dataPedido;
             DataEntregaPrevista = dataEntregaPrevista;
+            Opcionais = new List<Opcional>();
         }
 
         public Pedido(int idVeiculo, int idPacote, DateTime dataPedido, DateTime dataEntregaPrevista) {
+            //Construtor caso haja pacote
             IdVeiculo = idVeiculo;
             IdPacote = idPacote;
             DataPedido = dataPedido;
             DataEntregaPrevista = dataEntregaPrevista;
+            Opcionais = new List<Opcional>();
         }
 
     }
