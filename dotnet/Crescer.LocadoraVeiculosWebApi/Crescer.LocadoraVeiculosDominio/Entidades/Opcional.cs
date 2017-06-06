40a9b110243513e79bf4deb1dc1167056f1605d3
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-06 16:00:42 -0300

    feat(validação): adicionar interface de validar e verificar estoque dos opcionais.

--- dotnet/Crescer.LocadoraVeiculosWebApi/Crescer.LocadoraVeiculosDominio/Entidades/Opcional.cs
+++ dotnet/Crescer.LocadoraVeiculosWebApi/Crescer.LocadoraVeiculosDominio/Entidades/Opcional.cs
@@ -8,14 +8,36 @@ namespace Crescer.LocadoraVeiculosDominio.Entidades
 {
     public class Opcional
     {
-        public int Id { get; set; }
+        public int Id { get; private set; }
 
-        public string Descricao { get; set; }
+        public string Descricao { get; private set; }
 
-        public decimal Preco { get; set; }
+        public decimal Preco { get; private set; }
 
-        public int Quantidade { get; set; }
+        public int? Quantidade { get; private set; }
 
         protected Opcional() { }
+
+        public void diminuirEstoque()
+        {
+            if (Quantidade > 0) Quantidade--;
+
+            if (Quantidade == null)
+                throw new RecursoIlimitadoException("Esse opcional não pode ser decrescido, pois é um recurso ilimitado.");
+
+            else
+                throw new ForaDeEstoqueException("Não há estoque para esse opcional!");
+        }
+
+        public void aumentarEstoque ()
+        {
+            if (Quantidade >= 0) Quantidade++;
+
+            if (Quantidade == null)
+                throw new RecursoIlimitadoException("Esse opcional não pode receber mais unidades, pois é um recurso ilimitado.");
+                
+        }
+
+
     }
 }
