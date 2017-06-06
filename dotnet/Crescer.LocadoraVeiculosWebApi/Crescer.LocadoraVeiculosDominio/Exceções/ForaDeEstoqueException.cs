4d40e6fafac560e4d828e72db36095329e8c308d
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-06 16:01:10 -0300

    feat(exceptions): adicionar classe de exceção de fora do estoque e recurso ilimitado.

---
+++ dotnet/Crescer.LocadoraVeiculosWebApi/Crescer.LocadoraVeiculosDominio/Exceções/ForaDeEstoqueException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Crescer.LocadoraVeiculosDominio.Entidades
+{
+    internal class ForaDeEstoqueException : Exception
+    {
+        public ForaDeEstoqueException() { }
+
+        public ForaDeEstoqueException(string message) : base(message) { }
+
+        public ForaDeEstoqueException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
---
+++ dotnet/Crescer.LocadoraVeiculosWebApi/Crescer.LocadoraVeiculosDominio/Exceções/RecursoIlimitadoException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Crescer.LocadoraVeiculosDominio.Entidades
+{
+    internal class RecursoIlimitadoException : Exception
+    {
+        public RecursoIlimitadoException() { }
+
+        public RecursoIlimitadoException(string message) : base(message) { }
+
+        public RecursoIlimitadoException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
