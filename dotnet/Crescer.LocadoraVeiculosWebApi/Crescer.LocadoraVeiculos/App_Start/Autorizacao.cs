0c20a9120c274c844fccd7c1217be0182246e6db
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-06 13:35:22 -0300

    feat(autorização): adicionar classe de autorização.

---
+++ dotnet/Crescer.LocadoraVeiculosWebApi/Crescer.LocadoraVeiculos/App_Start/Autorizacao.cs
@@ -0,0 +1,102 @@
+﻿using Crescer.LocadoraVeiculosDominio.Entidades;
+using Crescer.LocadoraVeiculosInfraestrutura.Repositorio;
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace EditoraCrescer.Api.App_Start
+{
+    public class Autorizacao : AuthorizeAttribute
+    {
+        UsuarioRepositorio _usuarioRepositorio = new UsuarioRepositorio();
+
+        private static void negarAcesso(HttpActionContext actionContext)
+        {
+            var dnsHost = actionContext.Request.RequestUri.DnsSafeHost;
+
+            actionContext.Response =
+                actionContext
+                .Request
+                .CreateResponse(HttpStatusCode.Unauthorized);
+
+            actionContext
+                .Response
+                .Headers
+                .Add("WWW-Authenticate", string.Format("Basic realm=\"{0}\"", dnsHost));
+        }
+
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            if (actionContext.Request.Headers.Authorization == null)
+                negarAcesso(actionContext);
+
+            else
+            {
+
+                string tokenAutenticacao =
+                    actionContext.Request.Headers.Authorization.Parameter;
+
+                string decodedTokenAutenticacao =
+                    Encoding.Default.GetString(Convert.FromBase64String(tokenAutenticacao));
+
+                string[] userNameAndPassword = decodedTokenAutenticacao.Split(':');
+
+                Usuario usuario = null;
+                if (ValidarUsuario(userNameAndPassword[0], userNameAndPassword[1], out usuario))
+                {
+                    string[] papeis = usuario.Permissoes.Select(papel => papel.Nome).ToArray();
+                    var identidade = new GenericIdentity(usuario.Email);
+                    var genericUser = new GenericPrincipal(identidade, papeis);
+
+                    // confere o perfil da action com os do usuário
+                    if (string.IsNullOrEmpty(Roles))
+                    {
+                        // atribui o usuário informado no contexto da requisição atual
+                        Thread.CurrentPrincipal = genericUser;
+                        if (HttpContext.Current != null)
+                            HttpContext.Current.User = genericUser;
+
+                        return;
+                    }
+                    else
+                    {
+                        var currentRoles = Roles.Split(',');
+                        foreach (var currentRole in currentRoles)
+                        {
+                            if (genericUser.IsInRole(currentRole))
+                            {
+                                // atribui o usuário informado no contexto da requisição atual
+                                Thread.CurrentPrincipal = genericUser;
+                                if (HttpContext.Current != null)
+                                    HttpContext.Current.User = genericUser;
+
+                                return;
+                            }
+                        }
+                        negarAcesso(actionContext);
+                    }
+                }
+            }
+        }
+        private bool ValidarUsuario(string login, string senha, out Usuario usuarioRetorno)
+        {
+            usuarioRetorno = null;
+
+            var usuario = _usuarioRepositorio.Obter(login);
+
+            if (usuario != null && usuario.ValidarSenha(senha))
+                usuarioRetorno = usuario;
+            else
+                usuario = null;
+
+            return usuario != null;
+        }
+    }
+}
