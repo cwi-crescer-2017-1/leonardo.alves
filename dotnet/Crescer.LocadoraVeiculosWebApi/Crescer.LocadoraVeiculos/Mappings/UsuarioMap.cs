f968cc65f3daa12d0c0975097f70da411cb763c4
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-05 23:11:06 -0300

    feat(usuario): adicionar map, criptografia de senha e construtor.

---
+++ dotnet/Crescer.LocadoraVeiculosWebApi/Crescer.LocadoraVeiculos/Mappings/UsuarioMap.cs
@@ -0,0 +1,31 @@
+﻿using Crescer.LocadoraVeiculosDominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace Crescer.LocadoraVeiculos.Mappings
+{
+    public class UsuarioMap : EntityTypeConfiguration<Usuario>
+    {
+        public UsuarioMap()
+        {
+            ToTable("Usuario");
+
+            HasKey(x => x.Id);
+
+            Property(x => x.Email).IsRequired().HasMaxLength(130);
+            Property(x => x.Senha).IsRequired().HasMaxLength(130);
+
+            HasMany(x => x.Permissoes)
+                .WithMany()
+                .Map(x =>
+                {
+                    x.MapLeftKey("IdUsuario");
+                    x.MapRightKey("IdPermissao");
+                    x.ToTable("UsuarioPermissao");
+                });
+        }
+    }
+}
--- dotnet/Crescer.LocadoraVeiculosWebApi/Crescer.LocadoraVeiculosDominio/Entidades/Usuario.cs
+++ dotnet/Crescer.LocadoraVeiculosWebApi/Crescer.LocadoraVeiculosDominio/Entidades/Usuario.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,16 +11,29 @@ namespace Crescer.LocadoraVeiculosDominio.Entidades
     {
         public int Id { get; set; }
 
-        public int IdPermissao { get; set; }
+        public List<Permissao> Permissoes { get; set; }
 
-        public string Nome { get; set; }
-
+        public string Email { get; set; }
+        public string Senha { get; set; }        
         protected Usuario() { }
-
-        public Usuario(int idPermissao, string nome)
+        public Usuario(string email, string senha)
         {
-            IdPermissao = idPermissao;
-            Nome = nome;
+            Email = email;
+            if (!string.IsNullOrWhiteSpace(senha))
+                Senha = CriptografarSenha(senha);
+            Permissoes = new List<Permissao>();
+        }
+
+        private string CriptografarSenha(string senha)
+        {
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = Encoding.Default.GetBytes(Email + senha);
+            byte[] hash = md5.ComputeHash(inputBytes);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+                sb.Append(hash[i].ToString("x2"));
+
+            return sb.ToString();
         }
 
     }
