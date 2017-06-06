7c8305c590581e93b947fcfd1715752bf07bb889
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-05 23:11:35 -0300

    feat(permissao): adicionar map e classe.

---
+++ dotnet/Crescer.LocadoraVeiculosWebApi/Crescer.LocadoraVeiculos/Mappings/PermissaoMap.cs
@@ -0,0 +1,21 @@
+﻿using Crescer.LocadoraVeiculosDominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace Crescer.LocadoraVeiculos.Mappings
+{
+    public class PermissaoMap : EntityTypeConfiguration<Permissao>
+    {
+        public PermissaoMap()
+        {
+            ToTable("Permissao");
+
+            HasKey(x => x.Id);
+
+            Property(x => x.Nome).IsRequired().HasMaxLength(50);
+        }
+    }
+}
