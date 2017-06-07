ac186e8592083955bbf7f088d6a45e729343ff11
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-07 01:05:12 -0300

    refactor(model): remover model não utilizada.

--- dotnet/Crescer.LocadoraVeiculosWebApi/Crescer.LocadoraVeiculos/Models/EnderecoModel.cs
+++ dotnet/Crescer.LocadoraVeiculosWebApi/Crescer.LocadoraVeiculos/Models/EnderecoModel.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-
 namespace Crescer.LocadoraVeiculos.Models
 {
     public class EnderecoModel
--- dotnet/Crescer.LocadoraVeiculosWebApi/Crescer.LocadoraVeiculos/Models/GeneroModel.cs
+++ dotnet/Crescer.LocadoraVeiculosWebApi/Crescer.LocadoraVeiculos/Models/GeneroModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Crescer.LocadoraVeiculos.Models
+﻿namespace Crescer.LocadoraVeiculos.Models
 {
     public enum GeneroModel
     {
