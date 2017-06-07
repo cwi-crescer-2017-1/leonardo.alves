f9fa201dab84b23137a033574eb1e89297cc13eb
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-07 01:05:37 -0300

    feat(model): adicionar model para criar pedido.

---
+++ dotnet/Crescer.LocadoraVeiculosWebApi/Crescer.LocadoraVeiculos/Models/CriarPedidoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace Crescer.LocadoraVeiculos.Models
+{
+    public class CriarPedidoModel
+    {
+        public int IdCliente { get; set; }
+        public int IdVeiculo { get; set; }
+        public int? IdPacote { get; set; }
+        public int [] IdOpcional { get; set; }
+        public DateTime  DataEntrega { get; set; }   
+    }
+}
