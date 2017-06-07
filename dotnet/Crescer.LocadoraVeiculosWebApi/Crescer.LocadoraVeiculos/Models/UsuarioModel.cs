ab90c1130cffdd34dacf5261860bd8c4b50545d4
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-07 16:35:58 -0300

    feat(usuario): adicionar model.

---
+++ dotnet/Crescer.LocadoraVeiculosWebApi/Crescer.LocadoraVeiculos/Models/UsuarioModel.cs
@@ -0,0 +1,16 @@
+﻿using Crescer.LocadoraVeiculosDominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Crescer.LocadoraVeiculos.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get;  set; }        
+        public string Email { get; set; }
+        public string Senha { get; set; }
+
+    }
+}
