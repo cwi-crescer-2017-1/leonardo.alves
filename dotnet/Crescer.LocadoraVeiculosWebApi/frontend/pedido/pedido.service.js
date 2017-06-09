840616de88c1cb33b981c47f888770fb6f2ebe1f
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-09 03:52:53 -0300

    feat(pedido): adicionar funções de devolver pedido e gerar pedido com valor.

--- dotnet/Crescer.LocadoraVeiculosWebApi/frontend/pedido/pedido.service.js
+++ dotnet/Crescer.LocadoraVeiculosWebApi/frontend/pedido/pedido.service.js
@@ -8,16 +8,25 @@ function pedidoService($http) {
         getRelatorio: getRelatorio,
         getAtrasos: getAtrasos,
         criarPedido: criarPedido,
-        devolverPedido: devolverPedido
+        devolverPedido: devolverPedido,
+        gerarPedidoComValor: gerarPedidoComValor
     }
 
     function getPedidoPendente(cpf) {
-        return $http.get(url + "pendente", cpf);
+        return $http({
+            url: url + "pendente",
+            method: 'GET',
+            params: {cpf: cpf}
+        });
     }
 
     function getRelatorio(data) {
-        return $http.get(url + "relatorio", data);
-    }
+        return $http({
+            url: url + "relatorio",
+            method: 'GET',
+            params: {data: data.Data}
+        });
+    }   
 
     function getAtrasos() {
         return $http.get(url + "atrasos");
@@ -28,7 +37,27 @@ function pedidoService($http) {
     }
 
     function devolverPedido(id) {
-        return $http.put(url + "devolver", id);
+        return $http({
+            url: url + "devolver",
+            method: 'PUT',
+            params: {id: id}
+        });        
+    }
+
+     function gerarPedidoComValor(pedido){
+        let precoVeiculo = pedido.Veiculo.PrecoDiaria;
+        let precoAdicionalAtraso = pedido.atraso ? pedido.Veiculo.AdicionalDiaria : 0;
+        let precoPacote = pedido.Pacote.PrecoDiaria;        
+
+        if(typeof precoPacote == "undefined") precoPacote = 0;
+
+        let precoOpcionais = pedido.Opcionais.map(p => p.Opcional.Preco).reduce((a,b) => a+b);
+
+        let diferencaDeDias = moment().diff(moment(pedido.DataPedido), "days");
+
+        pedido.precoTotal = (diferencaDeDias || 1) * (precoVeiculo  + precoPacote + precoOpcionais + precoAdicionalAtraso);
+
+        return pedido;
     }
 
     return service;
