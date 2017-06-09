4e0b8f0dcfc9d76045ac754e1c487516c26d0e27
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-09 03:51:52 -0300

    feat(controllers): adicionar implementações em controller de relatorio e pedido.

--- dotnet/Crescer.LocadoraVeiculosWebApi/frontend/pedido/pedido.controller.js
+++ dotnet/Crescer.LocadoraVeiculosWebApi/frontend/pedido/pedido.controller.js
@@ -1,17 +1,18 @@
 angular.module('locadoraCrescer').controller("pedidoController", pedidoController);
 
 function pedidoController($scope, authService, pedidoService, opcionalService, pacoteService, veiculoService, clienteService) {
-    $scope.precoTotal = "Gere o pedido para visualizar o preço total.";
-    $scope.auth = authService;
-    $scope.logout = authService.logout;
+    $scope.precoTotal = "Gere o pedido para visualizar o preço total.";      
     $scope.gerarPedido = gerarPedido;
     $scope.reservar = reservar;
-    
+    $scope.getPendente = getPendente;
+    $scope.devolverPedido = devolverPedido;
+
     getOpcionais();
     getPacotes();
     getVeiculos();  
 
     function gerarPedido() {
+
         let precoVeiculo = $scope.pedido.Veiculo.PrecoDiaria;
         let precoPacote = $scope.pedido.Pacote.PrecoDiaria;
 
@@ -24,7 +25,7 @@ function pedidoController($scope, authService, pedidoService, opcionalService, p
                 return 0;            
         }).reduce((a,b) => a+b);
 
-        let diferencaDeDias = moment($scope.pedido.dataEntrega).diff(moment(new Date()), "days");
+        let diferencaDeDias = moment($scope.pedido.dataEntrega).diff(moment(), "days");
 
         $scope.precoTotal = diferencaDeDias * (precoVeiculo  + precoPacote + precoOpcionais);
     }  
@@ -46,11 +47,20 @@ function pedidoController($scope, authService, pedidoService, opcionalService, p
         })
     }
 
+    function devolverPedido () {
+        pedidoService.devolverPedido($scope.pendente.Id)
+            .then(response => {
+                alert(response.data.dados);
+            }, fail => {
+                alert(response.data.mensagens);
+            })
+        
+    }
+
     function getOpcionais() {
         opcionalService.getOpcionais()
             .then(response => {
-                $scope.opcionais = response.data.dados;
-                console.log($scope.opcionais);
+                $scope.opcionais = response.data.dados;                
             }, fail => {
                 console.log(fail.data.mensagens);
             });
@@ -82,4 +92,16 @@ function pedidoController($scope, authService, pedidoService, opcionalService, p
         }
         return idOpcionais;
     }
+
+    function getPendente(cpf) {
+        pedidoService.getPedidoPendente(cpf)
+            .then(response => {
+                $scope.pendente = pedidoService.gerarPedidoComValor(response.data.dados);
+                $scope.pendente.atraso = 
+                    moment($scope.pendente.DataEntregaPrevista).isBefore(moment());
+                
+            }, fail => {
+                alert(fail.data.mensagens);
+            });
+    }   
 }
--- dotnet/Crescer.LocadoraVeiculosWebApi/frontend/relatorio/relatorio.controller.js
+++ dotnet/Crescer.LocadoraVeiculosWebApi/frontend/relatorio/relatorio.controller.js
@@ -1,18 +1,37 @@
 angular.module('locadoraCrescer').controller("relatorioController", relatorioController);
 
-function relatorioController ($scope, pedidoService) {   
-   $scope.getRelatorioMensal = getRelatorioMensal;
+function relatorioController($scope, pedidoService, clienteService, opcionalService, pacoteService) {
+    $scope.getPedidosAtrasados = getPedidosAtrasados;
+    $scope.getRelatorioMensal = getRelatorioMensal;
+    $scope.pedidosMensais = [];    
+    $scope.lucroMensal = "Informe uma data.";    
 
+    function getRelatorioMensal(data) {
+        let dataCopia = angular.copy(data);
+        let date = moment(dataCopia.Data).format("YYYY-MM-DDTHH:mm:ss.SSS[Z]");
 
-    function getRelatorioMensal (data) {
-        pedidoService.getRelatorio(data.Data)
+        dataCopia.Data = date;
+        pedidoService.getRelatorio(dataCopia)
             .then(response => {
-                $scope.pedidos = response.data.dados; 
+                response.data.dados.forEach(d => {
+                    $scope.pedidosMensais.push(pedidoService.gerarPedidoComValor(d));
+                    $scope.lucroMensal = gerarLucroMensal($scope.pedidosMensais);
+                });
+            }, fail => {
+                alert(fail.data.mensagens);
+            });
+    }  
+
+    function getPedidosAtrasados () {
+        pedidoService.getAtrasos()
+            .then(response => {
+                $scope.pedidosAtrasados = response.data.dados;
             }, fail => {
                 alert(fail.data.mensagens);
             })
-    }
+    }  
 
+    function gerarLucroMensal(pedidos){
+        return pedidos.map(p => p.precoTotal).reduce((a,b) => a + b);
+    }
 }
-    
-    
