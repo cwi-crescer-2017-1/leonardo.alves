3f58d8d5ac2a19372da1ba0ac1fea3289eab9472
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-20 21:20:39 -0300

    feat(java-aula1): adicionar ex meu string utils.

---
+++ java/aula1/src/main/java/br/com/crescer/aula1/MeuStringUtils.java
@@ -0,0 +1,51 @@
+
+package br.com.crescer.aula1;
+
+import java.text.Normalizer;
+
+/** 
+ * @author leonardo.alves
+ **/
+public class MeuStringUtils implements StringUtils {
+    
+    private String normalizar (String string) {
+        return Normalizer
+                .normalize(string, Normalizer.Form.NFD)
+                .replaceAll("[^\\p{ASCII}]", "")
+                .toLowerCase();
+    }
+    
+    private String trim (String string) {
+        return string.replaceAll("\\s+","");
+    }
+
+    @Override
+    public boolean isEmpty(String string) {
+        return string.length() == 0 || string == null ;
+    }
+
+    @Override
+    public String inverter(String string) {
+        return new StringBuilder(string).reverse().toString();
+    }
+
+    @Override
+    public int contaVogais(String string) {
+        return  (int) normalizar(string)
+                    .chars()
+                    .mapToObj(c -> (char) c)
+                    .filter(s -> 
+                             s == 'a' || 
+                             s == 'e' ||
+                             s == 'i' ||
+                             s == 'o' ||
+                             s == 'u'   )
+                    .count();
+    }
+
+    @Override
+    public boolean isPalindromo(String string) {        
+        return trim(normalizar(string)).equals(trim(inverter(normalizar(string))));
+    }
+
+}
---
+++ java/aula1/src/main/java/br/com/crescer/aula1/StringUtils.java
@@ -0,0 +1,17 @@
+
+package br.com.crescer.aula1;
+
+/**
+ * @author carloshenrique
+ */
+public interface StringUtils {
+    
+    boolean isEmpty(String string);
+    
+    String inverter(String string);
+    
+    int contaVogais(String string);
+    
+    boolean isPalindromo(String string);
+    
+}
