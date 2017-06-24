8b0964ece5066d64ad958585f754e8bc12484f19
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-24 12:38:45 -0300

    feat(java-aula4): adicionar mapeamento do genero.

---
+++ java/aula4/src/main/java/br/com/crescer/aula4/exercicios/pojo/Genero.java
@@ -0,0 +1,60 @@
+
+package br.com.crescer.aula4.exercicios.pojo;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.SEQUENCE;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+
+/** 
+ * @author leonardo.alves
+ **/
+
+@Entity
+public class Genero implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = SEQUENCE, generator = "SEQ_GENERO")
+    @SequenceGenerator(name = "SEQ_GENERO", sequenceName = "SEQ_GENERO")
+    @Basic(optional = false)
+    private Long   id;  
+    
+    @Basic(optional = false)
+    private String descricao;
+    
+    @OneToMany(mappedBy = "genero", targetEntity = Video.class)
+    private List<Video> videos;    
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Video> getVideos() {
+        return videos;
+    }
+
+    public void setVideos(List<Video> videos) {
+        this.videos = videos;
+    }
+
+    
+    
+}
