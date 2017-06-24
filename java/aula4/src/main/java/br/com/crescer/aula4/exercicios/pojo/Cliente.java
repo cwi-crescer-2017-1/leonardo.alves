bc220e110ec135eeba0ee5a4ab79f2ea7791b9aa
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-24 12:38:13 -0300

    feat(java-aula4): adicionar mapeamento do cliente.

---
+++ java/aula4/src/main/java/br/com/crescer/aula4/exercicios/pojo/Cliente.java
@@ -0,0 +1,161 @@
+
+package br.com.crescer.aula4.exercicios.pojo;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.SEQUENCE;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/** 
+ * @author leonardo.alves
+ **/
+
+@Entity
+public class Cliente implements Serializable {
+    @Id
+    @GeneratedValue(strategy = SEQUENCE, generator = "SEQ_CLIENTE")
+    @SequenceGenerator(name = "SEQ_CLIENTE", sequenceName = "SEQ_CLIENTE")
+    @Basic(optional = false)
+    private Long   id;  
+    
+    @Basic(optional = false)
+    private String nome;    
+    
+    @Temporal(TemporalType.DATE)
+    private Date   nascimento; 
+    
+    @Basic(optional = false)
+    private String cpf;    
+    
+    private String rg;    
+    private String rua;    
+    private String bairro;    
+    private String cidade;
+    
+    @Column(name = "NUMERO_CASA")
+    private String numeroCasa;
+    
+    private String email;
+    private String telefone;
+    
+    @Basic(optional = false)
+    private String celular;
+    
+    @OneToMany(mappedBy = "cliente", targetEntity = Locacao.class)
+    private List<Locacao> locacoes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getNumeroCasa() {
+        return numeroCasa;
+    }
+
+    public void setNumeroCasa(String numeroCasa) {
+        this.numeroCasa = numeroCasa;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public List<Locacao> getLocacoes() {
+        return locacoes;
+    }
+
+    public void setLocacoes(List<Locacao> locacoes) {
+        this.locacoes = locacoes;
+    }
+    
+    
+}
