819fe3f925d46b97935521e28fa80b5c56211647
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-24 12:39:05 -0300

    feat(java-aula4): adicionar mapeamento da locação.

---
+++ java/aula4/src/main/java/br/com/crescer/aula4/exercicios/pojo/Locacao.java
@@ -0,0 +1,97 @@
+
+package br.com.crescer.aula4.exercicios.pojo;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.SEQUENCE;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/** 
+ * @author leonardo.alves
+ **/
+@Entity
+public class Locacao implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = SEQUENCE, generator = "SEQ_LOCACAO")
+    @SequenceGenerator(name = "SEQ_LOCACAO", sequenceName = "SEQ_LOCACAO")
+    @Basic(optional = false)
+    private Long id;
+    
+    @Temporal(TemporalType.DATE)
+    private Date dataDevolucao;
+    
+    @Column(name = "VALOR_TOTAL")
+    private Long valorTotal;    
+
+    @ManyToOne
+    @JoinColumn(name = "ID_CLIENTE")
+    private Cliente cliente;
+    
+    @ManyToOne
+    @JoinColumn(name = "ID_VIDEO")
+    private Video video;
+    
+    @ManyToOne
+    @JoinColumn(name = "ID_FUNCIONARIO")
+    private Funcionario funcionario;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(Date dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public Long getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Long valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Video getVideo() {
+        return video;
+    }
+
+    public void setVideo(Video video) {
+        this.video = video;
+    }
+
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+    
+    
+}
