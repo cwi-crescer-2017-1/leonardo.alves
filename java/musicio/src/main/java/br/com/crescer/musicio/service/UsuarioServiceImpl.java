d5b39fbf679fd0a71765af18f56bdbac75d4a259
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-30 12:32:06 -0300

    refactor(rede social): remover importações não utilizadas.

--- java/musicio/src/main/java/br/com/crescer/musicio/service/UsuarioServiceImpl.java
+++ java/musicio/src/main/java/br/com/crescer/musicio/service/UsuarioServiceImpl.java
@@ -4,15 +4,10 @@ package br.com.crescer.musicio.service;
 import br.com.crescer.musicio.entity.Usuario;
 import br.com.crescer.musicio.repository.UsuarioRepository;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
     
 public class UsuarioServiceImpl implements UsuarioService {
     
