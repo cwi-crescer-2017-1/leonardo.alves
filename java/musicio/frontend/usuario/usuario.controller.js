51046ea8a44c105100840c4c509903b51fb27eca
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-07-01 18:19:16 -0300

    feat(rede social): adicionar carregamento de post ao controlle de usuario.

--- java/musicio/frontend/usuario/usuario.controller.js
+++ java/musicio/frontend/usuario/usuario.controller.js
@@ -1,20 +1,61 @@
 app.controller("usuarioController", usuarioController);
 
 function usuarioController ($scope, $rootScope, usuarioService, authService, $location, postService) {
+    $scope.ultimaPagina = false;
     $scope.logar = logar;
+    $scope.numPagina = 0;
+    $scope.posts = [];
     $rootScope.isAutenticado = authService.isAutenticado();
     $rootScope.isHome = isHome;
     $rootScope.logout = authService.logout;   
 
     $scope.postar = postar;
+    $scope.carregarPosts = carregarPosts;
+
+    carregarPosts();
 
     function postar () {
 
         postService.addPost($scope.post)
             .then(response => {
-                alert("Postado");
+                $scope.post.texto = "";
+                atualizarPostsAposPostar();
             }, fail => {
                 alert("Não foi dessa vez.");
+            })
+    }
+
+    function carregarPosts () {
+        
+        postService.pegarPosts($scope.numPagina)
+            .then(response => {
+                 if (response.data.last) 
+                    $scope.ultimaPagina = true;
+                   
+                response.data.content.forEach(
+                   
+                    post => { 
+                       
+                        $scope.posts.push(post); 
+                    }                    
+                );
+                $scope.numPagina++;
+            }, fail => {
+                alert("Erro ao carregar");
+            });
+    }
+
+    function atualizarPostsAposPostar () {
+        $scope.posts = [];
+        postService.pegarPosts(0)
+            .then(response => {
+                response.data.content.forEach(
+                    post => {
+                        $scope.posts.push(post);
+                    }
+                );
+            }, fail => {
+                alert("Erro ao carregar");
             })
     }
 
