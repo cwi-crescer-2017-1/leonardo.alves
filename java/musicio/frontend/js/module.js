05825dd11f35c3739fb3f9c5645691aa44149104
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-07-01 18:17:28 -0300

    feat(rede social): adicionar modulo do tempo e rotas para a aplicação.

--- java/musicio/frontend/js/module.js
+++ java/musicio/frontend/js/module.js
@@ -1,11 +1,38 @@
-var app = angular.module("musicio", ['ngRoute']);
+var app = angular.module("musicio", ['ngRoute', 'auth', 'ngtimeago']);
 
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl: "home/home.html"
+        controller: "usuarioController",
+        templateUrl: "home/home.html",        
     })
     .when("/cadastro", {
         templateUrl: "usuario/cadastro.html"
     })
+    .when("/dashboard", {
+        templateUrl: "usuario/dashboard.html",
+        controller: "usuarioController",
+        resolve: {
+            autenticado: function  (authService)  {
+               return authService.isAutenticadoPromise();
+            }
+        }
+    })     
+});
+
+
+// Configurações utilizadas pelo módulo de autenticação (authService)
+app.constant('authConfig', {
+
+    // Obrigatória - URL da API que retorna o usuário
+    urlUsuario: 'http://localhost:9090/api/usuarios/currentUser',
+
+    // Obrigatória - URL da aplicação que possui o formulário de login
+    urlLogin: '/',
+
+    // Opcional - URL da aplicação para onde será redirecionado (se for informado) após o LOGIN com sucesso
+    urlPrivado: '/dashboard',
+
+    // Opcional - URL da aplicação para onde será redirecionado (se for informado) após o LOGOUT
+    urlLogout: '/'
 });
