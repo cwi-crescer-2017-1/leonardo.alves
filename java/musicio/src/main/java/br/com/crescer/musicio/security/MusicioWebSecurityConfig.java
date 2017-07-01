b038fc177a45d43cd305b849705c4f4dbf09c207
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-07-01 11:53:32 -0300

    feat(rede social): adicionar cors em web security config.

--- java/musicio/src/main/java/br/com/crescer/musicio/security/MusicioWebSecurityConfig.java
+++ java/musicio/src/main/java/br/com/crescer/musicio/security/MusicioWebSecurityConfig.java
@@ -2,13 +2,20 @@
 package br.com.crescer.musicio.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.savedrequest.NullRequestCache;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /** 
  * @author leonardo.alves
@@ -24,26 +31,36 @@ public class MusicioWebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .authorizeRequests()
-                    .antMatchers(HttpMethod.POST, "api/usuarios")
-                    .permitAll()
-                    .and()  
+                .authorizeRequests().antMatchers("/currentUser").permitAll()
+                .and()
+                .authorizeRequests().antMatchers(HttpMethod.POST, "/usuarios").permitAll()
+                .and()
                 .authorizeRequests().anyRequest().authenticated()
-                    .and()
-                    .httpBasic()
-                    .and()
-                .logout()
-                    .logoutUrl("/logout")
-                    .deleteCookies("JSESSIONID")
-                    .permitAll()
-                    .and()
-                .cors().disable()
+                .and()
+                .httpBasic()                                
+                .and()                
+                .cors()
+                .and()
                 .csrf().disable();
     }
-    
+
+    @Override
+    public void configure(WebSecurity webSecurity) throws Exception {
+        webSecurity.ignoring().antMatchers("/usuarios/login");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+
     @Autowired
     public void setDetailsService(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.userDetailsService(usuarioService);
+        auth.userDetailsService(usuarioService).passwordEncoder(new BCryptPasswordEncoder());
     }
 }
