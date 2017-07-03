895400e6ca1a01f43fe3aed4f1b69fd450998573
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-07-03 00:09:39 -0300

    feat(rede social): adicionar noty para alerts.

---
+++ java/musicio/frontend/js/alerts.js
@@ -0,0 +1,39 @@
+
+Noty.overrideDefaults({
+    layout: 'topRight',
+    theme: 'metroui',
+    timeout: 2000,
+    progressBar: true,
+    closeWith: [],
+    animation: {
+        open: 'noty_effects_open',
+        close: 'noty_effects_close'
+    },
+    buttons: []
+});
+
+var publicacaoPostada = new Noty({
+                        text: 'Publicação foi postada!',
+                        type: 'success'
+                    });
+                    
+var erroGenerico = new Noty({                    
+                    type: 'error'
+                });
+
+var recusarAmizade = new Noty({
+                    text: 'Solicitação recusada.'                    
+                });
+
+var aceitarAmizade = new Noty({
+                    text: 'Solicitação aceita.'                    
+                });
+
+var contaCriada = new Noty({
+                    text: 'Conta criada com sucesso',
+                    type: 'success'
+                });
+
+var boasVindas = new Noty({
+                    text:'Seja bem-vindo novamente!',                   
+                });
