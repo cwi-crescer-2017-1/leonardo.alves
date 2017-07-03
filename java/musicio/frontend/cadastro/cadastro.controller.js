f1493686214a57addbd9f80e0c111e217d7e423e
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-07-03 00:07:07 -0300

    feat(rede social): adicionar controller e pagina de cadastro.

---
+++ java/musicio/frontend/cadastro/cadastro.controller.js
@@ -0,0 +1,28 @@
+app.controller("cadastroController", cadastroController)
+
+function cadastroController ($scope, usuarioService, authService) {
+    $scope.cadastrar = cadastrar;
+
+    function cadastrar () {
+        if($scope.cadastro.$invalid) {
+            for(erro of $scope.cadastro.$error.required){
+                new Noty({
+                    text: erro.$name + " é obrigatório.",
+                    type: 'error'
+                }).show();
+            }
+           
+            return;
+        }
+
+        usuarioService.cadastrarUsuario($scope.usuario)
+            .then(response => {
+                authService.login($scope.usuario);
+                delete $scope.usuario;
+                contaCriada.show();
+            }, fail => { 
+                erroGenerico.setText(fail.data.message);
+                erroGenerico.show();
+            });
+    }
+}
