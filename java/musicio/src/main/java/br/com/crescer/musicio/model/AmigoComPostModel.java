3697baa220e0254a1e2b42fccec610c22c956a8b
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-07-03 01:49:33 -0300

    feat(rede social): adicionar model para post sem seu usuario.

---
+++ java/musicio/src/main/java/br/com/crescer/musicio/model/AmigoComPostModel.java
@@ -0,0 +1,31 @@
+
+package br.com.crescer.musicio.model;
+
+import br.com.crescer.musicio.entity.UsuarioBase;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+/** 
+ * @author leonardo.alves
+ **/
+public final class AmigoComPostModel extends UsuarioBase {
+
+    private List<PostSemUsuarioModel> posts;
+    
+    public AmigoComPostModel(BigDecimal idUsuario, String email, Character sexo, Date dataNascimento, List<PostSemUsuarioModel> posts) {
+        super(idUsuario, email, email, sexo, dataNascimento);
+        this.posts = posts;        
+    }
+
+    public List<PostSemUsuarioModel> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<PostSemUsuarioModel> posts) {
+        this.posts = posts;
+    }
+    
+    
+    
+}
