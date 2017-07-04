2eede841123d617df53a0a6dfde1749d962eadf7
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-07-04 11:51:01 -0300

    feat(rede social): adicionar play button do spotify.

--- java/musicio/frontend/pesquisa/musica.controller.js
+++ java/musicio/frontend/pesquisa/musica.controller.js
@@ -2,10 +2,10 @@ app.controller("musicaController", musicaController);
 
 
 
-function musicaController ($scope, $rootScope,$http, Spotify) {   
+function musicaController ($scope, $rootScope,$http, Spotify, $sce) {   
     $scope.params = [];
     $scope.procuraMusica = procuraMusica;
-    
+    $scope.urlMusica = urlMusica;
     if($rootScope.hash !== "" && typeof $rootScope.hash !== 'undefined'){
         let hash = angular.copy($rootScope.hash);
         $rootScope.hash = "";
@@ -22,5 +22,10 @@ function musicaController ($scope, $rootScope,$http, Spotify) {
                 console.log(response.data);
                
             });
-    }    
+    }  
+
+    function urlMusica(musica){       
+        let uri = "https://open.spotify.com/embed?uri=" + musica.uri + "&theme=white&view=coverart";
+        return $sce.trustAsResourceUrl(uri);
+    }  
 }
